deee8c9cbc69eea28f24faf50798dd3b47278bbb
eggcm <52985883+eggcm@users.noreply.github.com>
2024-06-18 21:13:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ function display() {
     // p.innerText = JSON.stringify(data[id]);
     const display = `
     SID:${data[id].學號}
+    GroupID:${data[id].Group}
     
     Homework Sets (30%)
     H1 (15%): ${data[id].HW1}
     H2 (15%): ${data[id].HW2}
 
     Project (65%)
-    GroupID:${data[id].Group}
     Proposal (20%):${data[id].Proposal}
     Final Demo(20%):${data[id].Final}
     Final Report(25%):${data[id].Report}
